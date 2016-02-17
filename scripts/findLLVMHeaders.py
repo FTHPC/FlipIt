a9a588a9a2ab76199aac5df6d3e6db6206115375
aperson40 <jon.calhoun42@gmail.com>
2016-02-16 21:23:22 -0600

    Remove sed's and pull llvm include dir from llvm-config

--- scripts/findLLVMHeaders.py
+++ scripts/findLLVMHeaders.py
@@ -20,11 +20,17 @@
 
 import os
 import sys
+import subprocess
 
+#get location of LLVM header files
 LLVM_BUILD_PATH = os.environ['LLVM_BUILD_PATH']
 #LLVM_REPO_PATH = os.environ['LLVM_REPO_PATH']
-
-# heade files used by the complier pass in 'faults.h'
+LLVM_REPO_PATH = subprocess.Popen(["llvm-config", "--includedir"],
+                            stdout=subprocess.PIPE).communicate()[0].strip()
+print LLVM_REPO_PATH
+if LLVM_REPO_PATH == "":
+    LLVM_REPO_PATH = LLVM_BUILD_PATH + "/include/"
+# header files used by the complier pass in 'faults.h'
 # might should grab this information from 'faults.h' and build at runtime.
 flipitHeaders = {'Pass.h': "#include <llvm\/Pass.h>",\
         'ArrayRef.h': "#include <llvm\/ADT\/ArrayRef.h>",\
@@ -51,7 +57,9 @@ flipitHeaders = {'Pass.h': "#include <llvm\/Pass.h>",\
 # replace header files in 'faults.h' with the correct headers for the version 
 #of LLVM at $LLVM_REPO_PATH
 filePath = sys.argv[1] #os.environ['FLIPIT_PATH'] + "/src/pass/faults.h"
-for path, subdirs, files in os.walk(LLVM_BUILD_PATH + "/include/llvm"):
+print LLVM_REPO_PATH + "/llvm"
+
+for path, subdirs, files in os.walk(LLVM_REPO_PATH + "/llvm"):
     for name in files:
         if name in flipitHeaders.keys():
             newInclude =  "#include <" + os.path.join( path[path.find("include")+8:], name) +">"
